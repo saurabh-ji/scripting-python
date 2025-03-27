0d0e951974a57a370e983232ea83876d96004a4a
saurabh yadav <saurabhcse70@gmail.com>
2025-03-27 11:52:55 +0000

    for loop script

---
+++ 14_forloop1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#loop for num
+for i in 1 2 3 4 5 6 7 8 9 10
+do 
+	echo "number is $i"
+done
+
+#loop for name 
+for  i in saurabh baba amit sumit pankaj
+do 
+	echo "name is $i"
+
+done
+
+#wild cards 
+
+for i in {1..100}
+do 
+	echo "number is  $i"
+
+done
