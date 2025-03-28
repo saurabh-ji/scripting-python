939b4c86d3e6dfe06db6a1bd160931a62455be03
saurabh yadav <saurabhcse70@gmial.com>
2025-03-28 08:28:40 +0000

    continue scripting

---
+++ 24_continue.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#Example of using continue in loop 
+#suppose we only need to print odd no.
+
+for i in 1 2 3 4 5 6 7 8 10
+do 
+   
+	let r=$i%2
+	if [[ $r -eq 0 ]]
+	then   
+		continue
+        fi		
+        echo "odd no is $i"
+done
