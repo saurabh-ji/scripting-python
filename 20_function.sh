52b9269e6d6bdfdadbe3f084450cfb6f6819ba79
saurabh yadav <saurabhcse70@gmial.com>
2025-03-28 07:39:52 +0000

    function script

---
+++ 20_function.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# to make function 
+
+function welcomenote {
+        echo "----------------"
+	echo "welcome"
+	echo "---------------"
+}
+
+#to call our function
+
+welcomenote
+welcomenote
+welcomenote
+
+
+# second step to create function 
+
+ welcomenote  () {
+        echo "----------------"
+        echo "welcome"
+        echo "---------------"
+}
+
+#to call our function
+
+welcomenote
+welcomenote
+welcomenote
+
+
