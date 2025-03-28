36668a91619bdf41fc0427d14ffa3bee3020e1b7
saurabh yadav <saurabhcse70@gmial.com>
2025-03-28 07:48:52 +0000

    script with functionarguments

---
+++ 21_function-with-arg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function  welcomenote {
+ 
+         echo "---------"
+	 echo "welcome $1"
+	 echo "age is $2"
+	 echo "---------"
+}
+
+welcomenote saurabh 23
+welcomenote pankaj 34
